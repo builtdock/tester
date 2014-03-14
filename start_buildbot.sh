29aa29076262e564053c398bb09697262f8c53c0
Matt Boersma <matt@opdemand.com>
2014-03-14 11:02:02 -0600

    Added slots for CLI builders/testers on different OSes.

--- start_buildbot.sh
+++ start_buildbot.sh
@@ -4,6 +4,10 @@ function echo_color {
   echo -e "\033[1m$1\033[0m"
 }
 
+function random_password {
+  echo `< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32`
+}
+
 # set up necessary environment variables
 : ${BUILDBOT_HOST:=`ifconfig | grep -Eo -m 1 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`}
 : ${BUILDSLAVE_ADMIN:=Your Name Here <admin@youraddress.invalid>}
@@ -11,14 +15,19 @@ function echo_color {
 : ${BUILDBOT_URL:=http://$BUILDBOT_HOST:8010/}
 : ${BUILDBOT_MASTER:=$BUILDBOT_HOST:9989}
 : ${REPO_PATH:=https://github.com/opdemand/deis.git}
-BUILDSLAVE1_USER=buildslave1
-BUILDSLAVE1_PASS=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32`
-BUILDSLAVE2_USER=buildslave2
-BUILDSLAVE2_PASS=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32`
+BUILDSLAVE1_USER=ubuntu-1
+BUILDSLAVE1_PASS=$(random_password)
+BUILDSLAVE2_USER=ubuntu-2
+BUILDSLAVE2_PASS=$(random_password)
+BUILDSLAVE3_USER=debian-1
+BUILDSLAVE3_PASS=$(random_password)
+BUILDSLAVE4_USER=macosx-1
+BUILDSLAVE4_PASS=$(random_password)
+BUILDSLAVE5_USER=windows-1
+BUILDSLAVE5_PASS=$(random_password)
 
 # start the buildbot master
 echo_color "Starting buildbot master..."
-# docker cp fca032bcd646:/app /
 docker run -d \
     -e REPO_PATH=$REPO_PATH \
     -e BUILDBOT_IRC_CHANNEL=$BUILDBOT_IRC_CHANNEL \
@@ -27,6 +36,12 @@ docker run -d \
     -e BUILDSLAVE1_PASS=$BUILDSLAVE1_PASS \
     -e BUILDSLAVE2_USER=$BUILDSLAVE2_USER \
     -e BUILDSLAVE2_PASS=$BUILDSLAVE2_PASS \
+    -e BUILDSLAVE3_USER=$BUILDSLAVE3_USER \
+    -e BUILDSLAVE3_PASS=$BUILDSLAVE3_PASS \
+    -e BUILDSLAVE4_USER=$BUILDSLAVE4_USER \
+    -e BUILDSLAVE4_PASS=$BUILDSLAVE4_PASS \
+    -e BUILDSLAVE5_USER=$BUILDSLAVE5_USER \
+    -e BUILDSLAVE5_PASS=$BUILDSLAVE5_PASS \
     -p :8010:8010 \
     -p :9989:9989 \
     -t deis/buildbot:latest
@@ -48,3 +63,17 @@ docker run -d \
   -e BUILDSLAVE_PASS=$BUILDSLAVE2_PASS \
   -e BUILDSLAVE_ADMIN="$BUILDSLAVE_ADMIN" \
   -t deis/buildbot-slave:latest
+# echo_color "Starting buildslave3..."
+# docker run -d \
+#   --privileged \
+#   -e BUILDBOT_MASTER=$BUILDBOT_MASTER \
+#   -e BUILDSLAVE_USER=$BUILDSLAVE3_USER \
+#   -e BUILDSLAVE_PASS=$BUILDSLAVE3_PASS \
+#   -e BUILDSLAVE_ADMIN="$BUILDSLAVE_ADMIN" \
+#   -t deis/buildbot-slave-debian:latest
+
+# for now, just echo the remaining generated passwords so we can connect
+# the other buildslaves manually.
+echo "$BUILDSLAVE3_USER uses password $BUILDSLAVE3_PASS"
+echo "$BUILDSLAVE4_USER uses password $BUILDSLAVE4_PASS"
+echo "$BUILDSLAVE5_USER uses password $BUILDSLAVE5_PASS"
